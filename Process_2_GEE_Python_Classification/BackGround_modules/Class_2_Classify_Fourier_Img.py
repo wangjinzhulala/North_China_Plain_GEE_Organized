5c15ca838ec969afec8d19ecb9a24032b30dd6a7
GEE_608583 <wangjinzhulala@qq.com>
2020-04-27 04:14:10 +1000

    Sample with Zone point

--- Process_2_GEE_Python_Classification/BackGround_modules/Class_2_Classify_Fourier_Img.py
+++ Process_2_GEE_Python_Classification/BackGround_modules/Class_2_Classify_Fourier_Img.py
@@ -19,7 +19,19 @@ ee.Initialize()
 
 
 
-# In[3]:
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[47]:
 
 
 class Classification:
@@ -38,7 +50,7 @@ class Classification:
     1) The [Input_img] is a string, like:
        [Input_img = "users/wang8052664/Cloud_Free_Img/Landsat_cloud_free_2017_2019"]
     2) The [year_name] is a string like '2017_2019'
-    3) the [input_band] defines the bands that are used in the classification
+    3) the [Input_band] defines the bands that are used in the classification
     4) the [Verified_point] defines the sample points as the ground truthes
     4) The default [Tree_num] is 100.
 
@@ -49,16 +61,18 @@ class Classification:
                             
     _____________________________An example of how to use this class____________________________
     
+    zone_sample = ee.FeatureCollection("users/Jinzhu_Deakin/North_China_Plain/Verified_points_with_zone_sample/Zone_Sample")
+    sample_2017_2019 = ee.FeatureCollection("users/wangjinzhulala/North_China_Plain_Python/Sample_Points/Verified_pt_2017_2019")
+    Verified_sample_with_zone = ee.FeatureCollection("users/Jinzhu_Deakin/North_China_Plain/Verified_points_with_zone_sample/Verified_point_2017_2019_with_zone_sample")
+
     # get necessary img and verified points
     imput_img = ee.Image("users/wang8052664/Cloud_Free_Img/Landsat_cloud_free_2017_2019")
-    input_band = imput_img.bandNames().getInfo()
-    Verified_point = ee.FeatureCollection("users/wangjinzhulala/North_China_Plain_Python/Sample_with_Landsat_Fourier_Nomalized_img_value/Sample_2017_2019_extract_Landsat_Fourier_Normalized_img")
 
     # Instatiate the class with a name.
     test = Classification(year_name ='2017_2019',
-                          Input_img  = "users/wang8052664/Cloud_Free_Img/Landsat_cloud_free_2017_2019",
-                          input_band = input_band,
-                          Verified_point = Verified_point)
+                          Input_img  = imput_img,
+                          Verified_point = sample_2017_2019,
+                          Zone_sample=zone_sample)
 
 
     # perform the classification on Input_img and Samples
@@ -71,7 +85,6 @@ class Classification:
     # Get the classified samples
     train_sample_classified        = test.Train_sample_classification
     test_sample_classified         = test.Test_sample_classification
-
     _______________________________________________________________________________________________
     
     
@@ -81,23 +94,20 @@ class Classification:
     
     def __init__(self,year_name,
                       Input_img,
-                      input_band,
                       Verified_point,
                       Tree_num      = 100,
-                      classProperty = 'Built'):
+                      classProperty = 'Built',
+                      Zone_sample   = None):
         
         self.Tree_num           = Tree_num
         self.Input_img          = ee.Image(Input_img)
         self.year_name          = year_name
-        self.Verified_point_all = ee.FeatureCollection(Verified_point)
-        self.input_band         = input_band
         self.classProperty      = classProperty
-        
-        
+        self.Verified_point_all = ee.FeatureCollection(Verified_point)
         
         #__________________________Split the point into built and non-built________________________________________
-        Verified_point_Built     =  self.Verified_point_all.filterMetadata(classProperty,'equals',1) 
-        Verified_point_non_Built =  self.Verified_point_all.filterMetadata(classProperty,'equals',0) 
+        Verified_point_Built     =  self.Verified_point_all.filterMetadata(classProperty,'equals',1)
+        Verified_point_non_Built =  self.Verified_point_all.filterMetadata(classProperty,'equals',0)
 
         # 70/30 Train/Test split on built/non-built points.
         Verified_built_pts_randomcolumn     = Verified_point_Built                                              .randomColumn(columnName = 'random',seed = 101) 
@@ -111,29 +121,111 @@ class Classification:
         Vetified_non_built_pts_test  = Verified_non_built_pts_randomcolumn                                      .filterMetadata('random','not_greater_than',0.3) 
 
         # Merge train/test datasets respectively.
-        self.Verified_pts_train = Vetified_built_pts_train.merge(Vetified_non_built_pts_train) 
+        if Zone_sample == None:
+            self.Verified_pts_train = Vetified_built_pts_train.merge(Vetified_non_built_pts_train)
+        else:  
+            self.Verified_pts_train = Vetified_built_pts_train.merge(Vetified_non_built_pts_train)                                                              .merge(Zone_sample)
+
+        
         self.Verified_pts_test  = Vetified_built_pts_test.merge(Vetified_non_built_pts_test)
         
         # ________________________________Train the classifier_______________________________________
+        # Create training points 
+        self.Verified_pts_train_with_img_value = self.Input_img.sampleRegions(collection = self.Verified_pts_train, 
+                                                                              properties = [self.classProperty], 
+                                                                              scale      = 30,
+                                                                              geometries = False)
+        
+        self.Verified_pts_test_with_img_value = self.Input_img.sampleRegions(collection = self.Verified_pts_test, 
+                                                                              properties = [self.classProperty], 
+                                                                              scale      = 30,
+                                                                              geometries = False)
+        
+        
         # Note that [smileRandomForest] is crazyly faster than [randomforest]!!!!
-
-        self.classifier = ee.Classifier.smileRandomForest(numberOfTrees = self.Tree_num)                                       .train(features        = self.Verified_pts_train,
-                                              inputProperties = self.input_band,
+        self.classifier = ee.Classifier.smileRandomForest(numberOfTrees = self.Tree_num)                                       .train(features        = self.Verified_pts_train_with_img_value,
+                                              inputProperties = self.Input_img.bandNames().getInfo(),
                                               classProperty   = self.classProperty)
         
         
     def Stp_1_Classification_on_img(self):
 
-        self.classification_img = self.Input_img.select(self.input_band).classify(self.classifier)
+        self.classification_img = self.Input_img.classify(self.classifier)
    
     
     def Stp_2_Classification_on_Samples(self):
         
         # Classify the Train-data set.
-        self.Train_sample_classification   = self.Verified_pts_train.select(self.input_band + [self.classProperty])                                                 .classify(self.classifier)
+        self.Train_sample_classification   = self.Verified_pts_train_with_img_value.classify(self.classifier)
         
         # Classify the Test-data set.
-        self.Test_sample_classification    = self.Verified_pts_test.select(self.input_band + [self.classProperty])                                                 .classify(self.classifier)
+        self.Test_sample_classification    = self.Verified_pts_test_with_img_value.classify(self.classifier)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[48]:
+
+
+zone_sample = ee.FeatureCollection("users/Jinzhu_Deakin/North_China_Plain/Verified_points_with_zone_sample/Zone_Sample")
+sample_2017_2019 = ee.FeatureCollection("users/wangjinzhulala/North_China_Plain_Python/Sample_Points/Verified_pt_2017_2019")
+Verified_sample_with_zone = ee.FeatureCollection("users/Jinzhu_Deakin/North_China_Plain/Verified_points_with_zone_sample/Verified_point_2017_2019_with_zone_sample")
+
+# get necessary img and verified points
+imput_img = ee.Image("users/wang8052664/Cloud_Free_Img/Landsat_cloud_free_2017_2019")
+
+# Instatiate the class with a name.
+test = Classification(year_name ='2017_2019',
+                      Input_img  = imput_img,
+                      Verified_point = sample_2017_2019,
+                      Zone_sample=zone_sample)
+
+
+# perform the classification on Input_img and Samples
+test.Stp_1_Classification_on_img()
+test.Stp_2_Classification_on_Samples()
+
+# Get the classified img
+classified_img = test.classification_img
+
+# Get the classified samples
+train_sample_classified        = test.Train_sample_classification
+test_sample_classified         = test.Test_sample_classification
+
+
+# In[ ]:
+
+
+
+
+
+# In[55]:
+
+
+from Class_3_Calculate_the_accuracy import Accuracy_assesment
+Accuracy_assesment(test_sample_classified).Stp_1_Calculate_Accuracy()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
