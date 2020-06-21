6e06802f6eedb2ebbf1ec4631e26b4b2b0939d67
wangjinzhulala <wangjinzhulala@qq.com>
2020-06-21 19:08:15 +1000

    using control points

--- Process_2_GEE_Python_Classification/BackGround_modules/Class_4_Create_Sample_Classification_instancs.py
+++ Process_2_GEE_Python_Classification/BackGround_modules/Class_4_Create_Sample_Classification_instancs.py
@@ -39,10 +39,10 @@ class Make_Sample_Classification():
         ('Fourier_Mean',<FeatureCollection with {Fourier and Mean_nomalized} values>)
         ('Landsat_Fourier_Mean',<FeatureCollection with {Landsat_Fourier_Mean} values>)]
                                         
-    function_3--> Create sample_classificaiton instances (aka, classified sample point) without computation in GEE
+    function_3--> Create sample_classificaiton instances (aka, classified sample point) without actual computation in GEE
       Note: need to from Class_2_Classify_Fourier_Img import Classification first to use this funcion
       In this function,:
-      1) the input {verified_pt} will be split into a 7/3 partition, the
+      1) the input {verified_pt} will be split into a 7/3 partition, then
       2) create a classifier (hidden in the Classificaiton class) from the 7 portion, and
       3) use the classsifier to classify the left 3 portion of sample points and ,
       4) return the classified sample with multiindex of (year,combo_name,percent,tree)
@@ -53,43 +53,39 @@ class Make_Sample_Classification():
     percent_value = [0,1,2,3,5,7,10,20,30,50,70,100]
     tree_num      = [1] + list(range(10,121,10))
 
-    # define the path to point_with_value GEE-Path
-    path = 'users/Jinzhu_Deakin/North_China_Plain/Sample_with_Landsat_Fourier_Normalized'
+    # define the path to point_with_img_value 
+    path = 'users/wangjinzhulala/North_China_Plain_Python/Sample_extract_img'
 
     # instantiate the combo_instance dictionary
     Combo_instance_with_village = {}
 
     # Create sample_classification instances through [year] --> [Percent] --> [band_combination] --> [Tree]
     for year in year_name:
-
-        # import samples
-        Verified_sample = ee.FeatureCollection(f'{path}/Verified_point_{year}_extract_Landsat_Fourier_Normalized_img')\
-                            .filterMetadata('Built', 'equals', 0)
-        Village_sample  = ee.FeatureCollection(f'{path}/Village_point_{year}_extract_Landsat_Fourier_Normalized_img')
-
-        # make sure they are the same size.
-        min_size = min(Verified_sample.size().getInfo(),Village_sample.size().getInfo())
-        Verified_sample = Verified_sample.randomColumn('z', 101).limit(min_size,'z')
-        Village_sample  = Village_sample.randomColumn('z', 101).limit(min_size,'z')
-
-        for pct in percent_reduction:
-
-            # Create percentage_reduced samples, only use non-built points from verified points
-            Subset_verified_sample  = Make_Sample_Classification.Step_1_Subset_sample(Verified_sample,pct)
-            Subset_village_sample   = Make_Sample_Classification.Step_1_Subset_sample(Village_sample,pct)
-
-            # Merge Verified_points with Zone_points
-            Sample_merge = Subset_verified_sample.merge(Subset_village_sample)
-
-            # Get the band_combo names
-            Band_combo   = Make_Sample_Classification.Step_2_Create_Band_Combo(Sample_merge)
-
-            for combo in Band_combo:
-                Accuracy_instance = Make_Sample_Classification.\
-                                    Step_3_Create_Classification_Instance(year,Sample_merge,
-                                                                          combo,tree_num,
-                                                                          pct,classificaiton_func = Classification ) 
-                Combo_instance_with_village.update(Accuracy_instance)
+    
+    Invarient_sample = ee.FeatureCollection(f'{path}/Control_sample_ext_img_{year}')
+    
+    # import samples
+    Invarient_built      = Invarient_sample.filterMetadata('Built','equals',1)
+    Invarient_non_Built  = Invarient_sample.filterMetadata('Built','equals',0)
+       
+    for pct in percent_value:
+        
+        # Create percentage_reduced samples, only use non-built points from verified points
+        Subset_Invarient_built     = Make_Sample_Classification.Step_1_Subset_sample(Invarient_built,pct)
+        Subset_Invarient_non_Built = Make_Sample_Classification.Step_1_Subset_sample(Invarient_non_Built,pct)
+        
+        # Merge the built and non-built points that been percentage reduced
+        Sample_merge = Subset_Invarient_built.merge(Subset_Invarient_non_Built)
+        
+        # Get the band_combo names
+        Band_combo   = Make_Sample_Classification.Step_2_Create_Band_Combo(Sample_merge)
+        
+        for combo in Band_combo:
+            Accuracy_instance = Make_Sample_Classification.\
+                                Step_3_Create_Classification_Instance(year,Sample_merge,
+                                                                      combo,tree_num,
+                                                                      pct,classificaiton_func = Classification ) 
+            Combo_instance_with_village.update(Accuracy_instance)
     
     '''
     
@@ -136,9 +132,9 @@ class Make_Sample_Classification():
                             Landsat_band + Mean_Normalized,
                             Fourier_band + Mean_Normalized,
                             Landsat_band + Fourier_band + Mean_Normalized,
-                            Landsat_band + Fourier_band + Climate_band,
-                            Landsat_band + Fourier_band + Terrain_band,
-                            Landsat_band + Fourier_band + Climate_band + Terrain_band]
+                            Landsat_band + Fourier_band + Mean_Normalized + Climate_band,
+                            Landsat_band + Fourier_band + Mean_Normalized + Terrain_band,
+                            Landsat_band + Fourier_band + Mean_Normalized + Climate_band + Terrain_band]
 
         # create comno_names
         global combination_name
@@ -148,9 +144,9 @@ class Make_Sample_Classification():
                             'Landsat_Mean',
                             'Fourier_Mean',
                             'Landsat_Fourier_Mean',
-                            'Landsat_Fourier_Climate',
-                            'Landsat_Fourier_Terrain',
-                            'Landsat_Fourier_Terrain_Climate']
+                            'Landsat_Fourier_Mean_Climate',
+                            'Landsat_Fourier_Mean_Terrain',
+                            'Landsat_Fourier_Mean_Terrain_Climate']
 
 
         return list(zip(combination_name,band_combination))
