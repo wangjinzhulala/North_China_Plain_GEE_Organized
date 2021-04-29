458266f07b9dbdc3442a1a047180a6d7af0ae129
wangjinzhulala <wangjinzhulala@qq.com>
2021-04-29 15:23:44 +1000

    add fiture-b to Area change Figure

--- Process_4_Making_standard_figs_use_R/R_Scripts/Section_2_Temporal_2_Area_change.R
+++ Process_4_Making_standard_figs_use_R/R_Scripts/Section_2_Temporal_2_Area_change.R
@@ -9,16 +9,26 @@ library(ggpubr)
 
 
 #________________________step 1: read data and format the df________________________
+#___1) area change data
 data.area_change = read.csv(paste("../../Process_2_Temporal_Check/",
                                    "Result/",
                                    "Area_change.csv",sep=""),
                              stringsAsFactors = T)%>% 
   mutate(year = paste0((year-1),'-',(year+1)))
 
+
+#___2) area percent_change_to_begining_year data
 data.area_change_percent = read.csv(paste("../../Process_2_Temporal_Check/",
                                   "Result/",
                                   "Area_change_percent.csv",sep=""),
                             stringsAsFactors = T)%>% 
+  mutate(year = paste0((year-1),'-',(year+1)))
+
+#___3) area percent_to_total_area_each_region data
+data.area_change_to_total_area = read.csv(paste("../../Process_2_Temporal_Check/",
+                                          "Result/",
+                                          "Area_percet_change_each_region.csv",sep=""),
+                                    stringsAsFactors = T)%>% 
   mutate(year = paste0((year-1),'-',(year+1)))
 
 
@@ -37,6 +47,12 @@ p_2_2_pct = data.area_change_percent %>%
   geom_point(size=1.5) +
   scale_color_discrete(breaks=c("Shandong","Henan","Hebei","Anhui","Jiangsu","Beijing","Tianjin"))
 
+p_2_2_total_region_portion = data.area_change_to_total_area %>% 
+  ggplot(aes(x=year,y=Proportion_total,color=EN_Name,group=EN_Name)) +
+  geom_line(size=0.5) +
+  geom_point(size=1.5) +
+  scale_color_discrete(breaks=c("Shandong","Henan","Hebei","Anhui","Jiangsu","Beijing","Tianjin"))
+
 plt_area_change = p_2_2 +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -51,9 +67,25 @@ plt_area_change = p_2_2 +
   labs(color = '',
        fill  = '',
        y = bquote('Area ('*10^5 ~km^2*')'),
-       x = '') # remove x label
+       x = '') + # remove x label 
+  guides(colour = guide_legend(nrow = 1))
 
 plt_area_change_pct = p_2_2_pct +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line.x.bottom = element_line(),
+        axis.text.x = element_blank(),
+        axis.line.y.left = element_line(),
+        legend.position = c(0.13, 0.75),
+        legend.key = element_rect(fill = NA ),
+        legend.background = element_blank()) +
+  labs(color = '',
+       fill  = '',
+       y = 'Percent (%)',
+       x = '')
+
+plt_area_pct_to_region = p_2_2_total_region_portion +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
@@ -65,23 +97,23 @@ plt_area_change_pct = p_2_2_pct +
         legend.background = element_blank()) +
   labs(color = '',
        fill  = '',
-       y = 'Percent Change(%)',
+       y = 'Built-up area proportion  (%)',
        x = 'Year')
 
 plt_area_pct = ggarrange(plt_area_change,
-                         plt_area_change_pct, 
+                         plt_area_pct_to_region,
                          align = 'v',
                          ncol=1, 
                          nrow=2, 
                          common.legend = TRUE, 
-                         legend="right",
+                         legend="bottom",
                          labels = c('a)','b)'),
                          label.x = 0.15,
                          label.y = 0.9)
 
 
 #______________________step 4: save plot to disk________________________
-plt_area_pct
+plt_area_pct 
 
 ggsave(plot = plt_area_pct,
        "../Section_2_2_Area_change.svg", 
